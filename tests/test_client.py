868fd27421bdbe7ac8b4761e0ec2308539ba6aec
Rafael Goncalves <rafaelgoncalvs@gmail.com>
2024-10-24 11:19:43 +1000

    fix tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -8,7 +8,7 @@ FROZEN_TIME = "2024-01-01T00:00:00.000Z"
 
 def test_client_class():
     client = Client()
-    assert client._base_url == "https://dev-api.cecil.earth/v0"
+    assert client._base_url == "https://dev-api.cecil.earth"
 
 
 @responses.activate
@@ -17,12 +17,12 @@ def test_client_create_data_request():
         responses.POST,
         "https://dev-api.cecil.earth/v0/data-requests",
         json={
-            "ID": "id",
-            "AOIID": "aoi_id",
-            "DatasetID": "dataset_id",
-            "SubRequests": [],
-            "Status": "processing",
-            "Created": FROZEN_TIME,
+            "id": "id",
+            "aoiId": "aoi_id",
+            "datasetId": "dataset_id",
+            "subRequests": [],
+            "status": "processing",
+            "created": FROZEN_TIME,
         },
         status=201,
     )
@@ -31,12 +31,12 @@ def test_client_create_data_request():
     res = client.create_data_request("aoi_id", "dataset_id")
 
     assert res == DataRequest(
-        ID="id",
-        AOIID="aoi_id",
-        DatasetID="dataset_id",
-        SubRequests=[],
-        Status="processing",
-        Created="2024-01-01T00:00:00.000Z",
+        id="id",
+        aoiId="aoi_id",
+        datasetId="dataset_id",
+        subRequests=[],
+        status="processing",
+        created="2024-01-01T00:00:00.000Z",
     )
 
 
@@ -46,22 +46,22 @@ def test_client_list_data_requests():
         responses.GET,
         "https://dev-api.cecil.earth/v0/data-requests",
         json={
-            "Records": [
+            "records": [
                 {
-                    "ID": "data_request_id_1",
-                    "AOIID": "aoi_id",
-                    "DatasetID": "dataset_id",
-                    "SubRequests": [],  # TODO: Add some SubRequests
-                    "Status": "processing",
-                    "Created": "2024-09-19T04:45:57.561Z",
+                    "id": "data_request_id_1",
+                    "aoiId": "aoi_id",
+                    "datasetId": "dataset_id",
+                    "subRequests": [],  # TODO: Add some SubRequests
+                    "status": "processing",
+                    "created": "2024-09-19T04:45:57.561Z",
                 },
                 {
-                    "ID": "data_request_id_2",
-                    "AOIID": "aoi_id",
-                    "DatasetID": "dataset_id",
-                    "SubRequests": [],  # TODO: Add some SubRequests
-                    "Status": "completed",
-                    "Created": "2024-09-19T04:54:38.252Z",
+                    "id": "data_request_id_2",
+                    "aoiId": "aoi_id",
+                    "datasetId": "dataset_id",
+                    "subRequests": [],  # TODO: Add some SubRequests
+                    "status": "completed",
+                    "created": "2024-09-19T04:54:38.252Z",
                 },
             ]
         },
@@ -72,19 +72,19 @@ def test_client_list_data_requests():
 
     assert data_requests == [
         DataRequest(
-            ID="data_request_id_1",
-            AOIID="aoi_id",
-            DatasetID="dataset_id",
-            SubRequests=[],
-            Status=DataRequestStatus.PROCESSING,
-            Created="2024-09-19T04:45:57.561Z",
+            id="data_request_id_1",
+            aoiId="aoi_id",
+            datasetId="dataset_id",
+            subRequests=[],
+            status=DataRequestStatus.PROCESSING,
+            created="2024-09-19T04:45:57.561Z",
         ),
         DataRequest(
-            ID="data_request_id_2",
-            AOIID="aoi_id",
-            DatasetID="dataset_id",
-            SubRequests=[],
-            Status=DataRequestStatus.COMPLETED,
-            Created="2024-09-19T04:54:38.252Z",
+            id="data_request_id_2",
+            aoiId="aoi_id",
+            datasetId="dataset_id",
+            subRequests=[],
+            status=DataRequestStatus.COMPLETED,
+            created="2024-09-19T04:54:38.252Z",
         ),
     ]
